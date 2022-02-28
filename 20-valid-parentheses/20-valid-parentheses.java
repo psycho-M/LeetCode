558ffa54b71d83fa95792e836c8404ed2517fede
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-02-28 16:34:59 +0530

    Time: 1 ms (99.40%), Space: 40.5 MB (41.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        /*
-            A classic stack problem of valid parentheses problem.
-            Adding openning brackets into the stack when encountered.
-            When closing brackets encountered, check if the stack is not empty
-            and top element of stack id the corresponding opening bracket. 
-            If it satisfies both the conditions, we pop the top element, else
-            we return false.
-        */
+        
         char[] ch = s.toCharArray();
         Stack<Character> stack = new Stack<>();
         
