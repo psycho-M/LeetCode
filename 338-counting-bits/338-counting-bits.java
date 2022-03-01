e605d7619b422476cb5bbd0fb94078041d0dd649
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-01 07:23:42 +0530

    Time: 3 ms (57.63%), Space: 48 MB (36.80%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -4,13 +4,13 @@ class Solution {
         ans[0] = 0;
         int temp, count;
         for(int i = 1; i <= n; i++) {
-            temp = i;
-            count = 0;
-            while(temp > 0) {
-                count += temp % 2 == 0 ? 0 : 1;
-                temp = temp>>1;
-            }
-            ans[i] = count;
+            // temp = i;
+            // count = 0;
+            // while(temp > 0) {
+            //     count += temp % 2 == 0 ? 0 : 1;
+            //     temp = temp>>1;
+            // }
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         
         return ans;
