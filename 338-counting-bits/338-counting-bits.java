9d834fbd46f00092050d3534f125c980081d8a2c
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-01 07:19:04 +0530

    Time: 12 ms (14.95%), Space: 48.1 MB (33.24%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,9 +2,10 @@ class Solution {
     public int[] countBits(int n) {
         int ans[] = new int[n + 1];
         ans[0] = 0;
+        int temp, count;
         for(int i = 1; i <= n; i++) {
-            int temp = i;
-            int count = 0;
+            temp = i;
+            count = 0;
             while(temp > 0) {
                 count += temp % 2 == 0 ? 0 : 1;
                 temp = temp>>1;
