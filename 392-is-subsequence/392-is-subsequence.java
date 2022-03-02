875f7aeec3189d2d88bf7994ee4d4a5d4f283a71
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-02 06:26:39 +0530

    Time: 1 ms (88.36%), Space: 42.2 MB (23.44%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,4 +1,16 @@
 class Solution {
+    /*
+        A two pointer problem.
+        First we create an int array of length 26 (to store frequency of the letters), alph, and
+        iterate over s char array to get store the frequencies of the letters into alph.
+        Now we use two pointers, one on the strting t, the other on the charArray of s.
+        Then compare the letters at the current pointer positions of s and t.
+        if same
+            we check if the frequency of that letter is greater than zero in alph
+            if yes
+                we reduce the frequency of that
+    */
+    
     public boolean isSubsequence(String s, String t) {
         int tLen = t.length(), sLen = s.length();
         
@@ -13,7 +25,7 @@ class Solution {
         int count = s.length();
         int i = 0;
         for(char c : t.toCharArray()) {
-            if(i < sLen && c == sChar[i] && sChar[i] > 0) {
+            if(i < sLen && c == sChar[i]) {
                 alph[c - 'a']--;
                 i++;
             }
