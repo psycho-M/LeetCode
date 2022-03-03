626a35db94428cd6cc0e110f277c08cb569a93fd
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-03 12:04:59 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (37.06%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -19,10 +19,10 @@ class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         if(nums.length < 3) return 0;
         
-        int count = 0, i, j;
+        int count = 0, i, j, diff;
         
         for(i = 0; i < nums.length - 1; i++) {
-            int diff = nums[i + 1] - nums[i];
+            diff = nums[i + 1] - nums[i];
             for(j = i + 1; j < nums.length - 1; j++) {
                 if(nums[j + 1] - nums[j] != diff) {
                     break;
