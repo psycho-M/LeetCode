eff53dde4d9f3ed55fa1f74d6e5f50ad64f3c6be
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-03 12:04:39 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (43.75%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,4 +1,21 @@
 class Solution {
+    /*
+        In this problem we observe that a subarray, with a length of >= 3, will have
+            (n * (n + 1)) / 2
+        subarrays that satisfies the condition for arithmetic array given in the problem.
+        where n = (j - i - 1),
+        i = start index of subarray,
+        j = end index of subarray
+        
+        Ex: [1,2,3,4,3,2,1,0]
+        First subarray: i = 0, j = 3, n = 2, Ans = (2 * 3) / 2 = 3
+        Second subarray: i = 3, j = 7, n = 3, Ans (3 * 4) / 2 = 6
+        
+        Final ans = 3 + 6 = 9
+        
+        I don't know DP yet, so idk what to call this approach.
+    */
+    
     public int numberOfArithmeticSlices(int[] nums) {
         if(nums.length < 3) return 0;
         
