6d205ea6ea7a12287279ba0706769ca611e1a9f6
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-08 09:57:28 +0530

    Time: 0 ms, Memory: 49 MB - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -1,6 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        reverse(s, 0, s.length - 1);
+        // reverse(s, 0, s.length - 1);
+        if(s.length == 1) return;
+        int i = 0, j = s.length - 1;
+        while(i < j) {
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
+        
     }
     
     public void reverse(char[] s, int left, int right) {
