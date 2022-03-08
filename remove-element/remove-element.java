6bc01c937eded981275fdf507cd532e0b28bd1eb
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-09 01:10:09 +0530

    Time: 0 ms, Memory: 43 MB - LeetHub

--- remove-element/remove-element.java
+++ remove-element/remove-element.java
@@ -1,4 +1,15 @@
 class Solution {
+    /*
+        A Two pointer appraoch for inplace modification of the array.
+        
+        Here we need to remove all the numbers equal to val from array.
+        What we do is, we use a slow and a fast pointer.
+        We increment fast and slow by one every time we encounter a fast-th element that is not
+        equal to val. And put the fast element in place of the slow-th index of array.
+        But when we encounter a fast-th element that is equal to val, we only increment fast by one, and
+        not slow.
+    */
+    
     public int removeElement(int[] nums, int val) {
         int slow = 0, fast = 0;
         while(fast < nums.length) {
