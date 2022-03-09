42222e6b714981725902cfb26aaf6e1235e3b831
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-09 07:31:28 +0530

    Time: 1 ms (77.24%), Space: 44.1 MB (13.77%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -9,25 +9,47 @@
  * }
  */
 class Solution {
+
+    /*
+        A LL question we have to delete all nodes that have duplicates.
+        
+        We use 2 pointers, a curr pointer and a prev pointer to keep track of teh element before curr.
+        We check 
+        if curr.val is equal to curr.next.val,
+            if yes,
+                we check if curr is head, we then keep on moving the head pointer forward until the head 
+                and its next element are diff, and then assign head to curr.
+            else 
+                we keep moving the curr pointer forward until the curr and its next elemetn are diff, and then we
+                assign curr.next to prev.next. We don't change the prev to curr here as it isn't needed.
+        else 
+            we assign curr to prev and then move curr to next node.
+        
+        
+        
+    */
+    
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null) return head;
         
         ListNode prev = head, curr = head, currNext = head.next;
         while(curr != null && curr.next != null) {
             if(curr.val == curr.next.val) {
-                if(curr == head) {
+                if(curr == head) { //checking if head is duplicate
+                    
                     while(head.next != null && head.val == head.next.val) {
                         head = head.next;
                     }
+                    
                     if(head.next == null) return null;
+                    
                     head = head.next;
                     prev = curr = head;
                 } else {
+                    
                     while(curr.next != null && curr.val == curr.next.val) {
                         curr = curr.next;
                     }
-                    
-                    // if(curr.next == null) return head;
                     
                     prev.next = curr.next;
                     curr = curr.next;
