e11e19a28f60e91bc1e178ab4c7325c01ac6f1a7
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-10 07:14:35 +0530

    Time: 5 ms (18.16%), Space: 47.4 MB (47.64%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -77,63 +77,24 @@ class Solution {
       
 //         return head;
         
-//         ListNode res = new ListNode(0);
+        ListNode res = new ListNode(0);
         
-//         ListNode temp = res;
-//         int carry = 0;
-//         while(l1 != null || l2 != null) {
-//             int a = l1 != null ? l1.val : 0;
-//             int b = l2 != null ? l2.val : 0;
-//             int sum = a + b + carry;
-//             carry = sum / 10;
-//             temp.next = new ListNode(sum % 10);
-//             temp = temp.next;
-//             if(l1 != null) l1 = l1.next;
-//             if(l2 != null) l2 = l2.next;
-//         }
-        
-//         if(carry > 0) temp.next = new ListNode(carry);
-        
-//         return res.next;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        ListNode res = new ListNode(0), head = res;
-        int carry = 0, x, y, temp;
+        ListNode temp = res;
+        int carry = 0;
         while(l1 != null || l2 != null) {
-            x = l1 == null ? 0 : l1.val;
-            y = l2 == null ? 0 : l2.val;
-            temp = x + y + carry;
-            carry = temp / 10;
-            res.next = new ListNode(temp % 10);
-            res = res.next;
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
             if(l1 != null) l1 = l1.next;
             if(l2 != null) l2 = l2.next;
         }
         
-        if(carry > 0) res.next = new ListNode(carry);
-        return head.next;
+        if(carry > 0) temp.next = new ListNode(carry);
+        
+        return res.next;
         
     }
 }
