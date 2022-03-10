346a0470b78aab0cf6597aad4aec20de67109d91
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-10 13:00:55 +0530

    Time: 1 ms, Memory: 47.6 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,12 +3,9 @@ class Solution {
         if(nums.length == 1) return 1;
         int i = 0, j = 1;
         while(j < nums.length) {
-            if(nums[i] == nums[j]) j++;
-            else {
-                nums[++i] = nums[j];
-            }
+            if(nums[i] == nums[j]) ++j;
+            else nums[++i] = nums[j];
         }
-        
         return i + 1;
     }
 }
