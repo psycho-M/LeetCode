0cd05922d7389b7395f3e8feb965680e8e457f37
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-17 09:49:38 +0530

    Time: 25 ms (33.99%), Space: 48.4 MB (12.63%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("\\p{Punct}", "").replaceAll(" ", "").toLowerCase();
-        StringBuffer res = new StringBuffer(s);
-        res = res.reverse();
-        return res.toString().equals(s);
+        String rev = new StringBuffer(s).reverse().toString();
+        return rev.toString().equals(s);
     }
 }
