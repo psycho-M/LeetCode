f123e9675901a38054c1c730feb186a42c52cd15
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-24 07:44:28 +0530

    Time: 24 ms (26.37%), Space: 65.1 MB (60.12%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int len = people.length, count = 0, i = 0, j = len - 1, curr;
+        while(i < j) {
+            if(people[j] == limit) {
+                j--;
+            } else {
+                curr = people[i] + people[j];
+                if(curr <= limit) {
+                    i++;
+                }
+                j--;
+            }
+            count++;
+        }
+        if(i == j) return ++count;
+        return count;
+    }
+}
