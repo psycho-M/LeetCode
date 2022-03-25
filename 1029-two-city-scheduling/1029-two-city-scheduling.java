7d70682e72138c8dfd0f9929b57dd4e12581b1b4
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-25 06:59:11 +0530

    Time: 1 ms (99.54%), Space: 42.4 MB (40.68%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,14 +1,14 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int cost = 0;
-        int[] refund = new int[costs.length];
+        int[] refund = new int[costs.length]; //refund bth - ath
         for(int i = 0; i < costs.length; i++) {
-            cost += costs[i][0];
+            cost += costs[i][0]; //sending all to a
             refund[i] = costs[i][1] - costs[i][0];
         }
-        Arrays.sort(refund);
+        Arrays.sort(refund); //sorting to know whom all to send to b
         for(int i = 0; i < costs.length / 2; i++) {
-            cost += refund[i];
+            cost += refund[i]; // correcting the costs
         }
         return cost;
     }
