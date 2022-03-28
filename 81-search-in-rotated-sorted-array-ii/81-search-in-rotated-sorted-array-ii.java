1dfc9eca21d907580d28b3f45682555da4861d69
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-28 07:35:20 +0530

    Time: 2 ms (12.37%), Space: 44 MB (37.77%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,14 +1,33 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        if(nums.length == 1) return nums[0] == target;
-        Arrays.sort(nums);
+    public boolean search(int[] nums, int t) {
+        if(nums.length == 1) return nums[0] == t;
         int l = 0, r = nums.length - 1, m;
         while(l <= r) {
-            m = (l + r) /  2;
-            if(nums[m] == target) return true;
-            else if(nums[m] < target) l = m + 1;
-            else r = m - 1;
+            m = (l + r) / 2;
+            if(nums[m] == t) return true;
+            if(nums[l] < nums[m]) {
+                if(t < nums[l] || t > nums[m]) l = m + 1;
+                else r = m - 1;
+            } else if(nums[l] > nums[m]) {
+                if(t < nums[m] || t > nums[r]) r = m - 1;
+                else l = m + 1;
+            } else l++;
         }
         return false;
+        
+        
+        
+        
+        // if(nums.length == 1) return nums[0] == target;
+        // Arrays.sort(nums);
+        // int l = 0, r = nums.length - 1, m;
+        // while(l <= r) {
+        //     m = (l + r) /  2;
+        //     if(nums[m] == target) return true;
+        //     else if(nums[m] < target) l = m + 1;
+        //     else r = m - 1;
+        // }
+        // return false;
+        
     }
 }
