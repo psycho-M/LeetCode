ad741191b4bf0cf58f99f8c7f7ffc56d3ec91e52
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-31 00:38:40 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (13.80%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+      while(i < matrix.length && j >= 0) {
+        if(matrix[i][j] == target)
+          return true;
+        else if(matrix[i][j] > target)
+          j --;
+        else if(matrix[i][j] < target)
+          i ++;
+      }
+      return false;
+    }
+    
+    boolean binarySearch(int arr[], int target) {
+        int l = 0, r = arr.length - 1, m;
+        while(l < r) {
+            m = (l + r) / 2;
+            if(arr[m] == target) return true;
+            else if(arr[m] > target) r = m - 1;
+            else l = m + 1;
+        }
+        return false;
+    }
+}
