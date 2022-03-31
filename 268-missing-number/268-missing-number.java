7e44196cabe9266c714253497dc1d330a52916bb
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-03-31 16:03:06 +0530

    Time: 5 ms (27.99%), Space: 43.4 MB (81.61%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,12 +1,20 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int[] map = new int[nums.length + 1];
-        for(int n : nums) map[n]++;
+        Arrays.sort(nums);
         
-        for(int i = 1; i < map.length; i++) {
-            if(map[i] == 0) return i;
+        for(int i = 0; i < nums.length; i++) {
+            if(i != nums[i]) return i;
         }
         
-        return 0;
+        return nums.length;
+        
+//         int[] map = new int[nums.length + 1];
+//         for(int n : nums) map[n]++;
+        
+//         for(int i = 1; i < map.length; i++) {
+//             if(map[i] == 0) return i;
+//         }
+        
+//         return 0;
     }
 }
