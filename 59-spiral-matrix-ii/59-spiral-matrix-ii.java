36451a0bf3187c846ad57559b931b5ba0d338852
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-04-13 08:47:37 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (90.73%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int count = 1;
+        int iUBound = n - 1, iLBound = 0, jUBound = n - 1, jLBound = 0, i = 0, j = 0;
+        while(iUBound >= iLBound && jUBound >= jLBound) {
+            for(j = jLBound; j <= jUBound; j++) {
+                res[i][j] = count;
+                count++;
+            }
+            
+            j--;
+            if(iLBound++ == iUBound) break;
+            
+            for(i = iLBound; i <= iUBound; i++) {
+                res[i][j] = count;
+                count++;
+            }
+            
+            i--;
+            if(jUBound-- == jLBound) break;
+            
+            for(j = jUBound; j >= jLBound; j--) {
+                res[i][j] = count;
+                count++;
+            }
+            
+            j++;
+            if(iUBound-- == iLBound) break;
+            
+            for(i = iUBound; i >= iLBound; i--) {
+                res[i][j] = count;
+                count++;
+            }
+            
+            i++;
+            jLBound++;
+            
+        }
+        return res;
+    }
+}
