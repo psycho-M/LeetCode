87f1ed39b8e5713cfae7eeb207f10636fd822c26
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-04-21 07:16:37 +0530

    Time: 60 ms (30.91%), Space: 111.7 MB (5.65%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,29 @@
+class MyHashSet {
+    
+    int[] set;
+    int limit = 1000001;
+    public MyHashSet() {
+        set = new int[limit];
+    }
+    
+    public void add(int key) {
+        if(set[key % limit] == 0) set[key % limit] = 1;
+    }
+    
+    public void remove(int key) {
+        if(set[key % limit] == 1) set[key % limit] = 0;
+    }
+    
+    public boolean contains(int key) {
+        if(set[key % limit] == 1) return true;
+        else return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
