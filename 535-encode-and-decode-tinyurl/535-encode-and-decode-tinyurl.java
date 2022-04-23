4a7528f46589a2fad7cb7d66e5fd6208e53f028e
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-04-23 06:55:45 +0530

    Time: 18 ms (8.79%), Space: 44.8 MB (5.19%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,14 +1,30 @@
 public class Codec {
+    Map<String, String> map = new HashMap<>();
     String origUrl;
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
         origUrl = longUrl;
-        return origUrl;
+        // return origUrl;
+        StringBuilder sb = new StringBuilder("https://tinyurl.com/");
+        String[] str = longUrl.split("/");
+        // print(str);
+        for(int i = 4; i < str.length; i++) 
+            sb.append(str[i].length() + str[i].substring(0, 1));
+        
+        String shortUrl = sb.toString();
+        map.put(shortUrl, longUrl);
+        return shortUrl;
+        
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return origUrl;
+        return map.get(shortUrl);
+    }
+    
+    void print(String[] str) {
+        for(String s : str) System.out.print(s + ", ");
+        System.out.println();
     }
 }
 
