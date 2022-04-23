fd6ef35b52bdb6011e358fa2cb7b7bb379c7ec2a
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-04-23 06:14:57 +0530

    Time: 18 ms (87.44%), Space: 61.8 MB (55.13%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,22 +1,58 @@
 class MyHashMap {
     
-    int[] map;
+    // int[] map;
+    Node[] map;
     
     public MyHashMap() {
-        map = new int[1000001];
-        Arrays.fill(map, -1);
+        // map = new int[1000001];
+        // Arrays.fill(map, -1);
+        map = new Node[10000];
     }
     
-    public void put(int key, int value) {
-        map[key] = value;
+    int getIndex(int key) {
+        return Integer.hashCode(key) % map.length;
+    }
+    
+    Node findElement(int index, int key) {
+        if(map[index] == null) return map[index] = new Node(-1, -1);
+        Node prev = map[index];
+        
+        while(prev.next != null && prev.next.key != key) prev = prev.next;
+        return prev;
+    }
+    
+    public void put(int key, int val) {
+        // map[key] = value;
+        int index = getIndex(key);
+        Node prev = findElement(index, key);
+        
+        if(prev.next == null) prev.next = new Node(key, val);
+        else prev.next.val = val;
     }
     
     public int get(int key) {
-        return map[key];
+        // return map[key];
+        int index = getIndex(key);
+        Node prev = findElement(index, key);
+        return prev.next == null ? -1 : prev.next.val;
     }
     
     public void remove(int key) {
-        map[key] = -1;
+        // map[key] = -1;
+        int index = getIndex(key);
+        Node prev = findElement(index, key);
+        
+        if(prev.next != null) prev.next = prev.next.next;
+    }
+}
+
+class Node {
+    int key, val;
+    Node next;
+    
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
     }
 }
 
