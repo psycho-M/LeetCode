b89de6c47f8afd886138b1e410d2c50bb56215e5
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-04-25 18:52:22 +0530

    Time: 42 ms (5.15%), Space: 55.2 MB (80.62%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,10 +1,37 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int n : nums) {
-            if(set.contains(n)) return true;
-            set.add(n);
+        int[] res = new int[nums.length];
+        topDownMergeSort(nums, 0, nums.length, res);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) return true;
         }
         return false;
     }
+    
+    void topDownMergeSort(int[] nums, int l, int r, int[] res) {
+        if(r - l != 1) {
+            int m = (r + l) / 2;
+            topDownMergeSort(nums, l, m, res);
+            topDownMergeSort(nums, m, r, res);
+            
+            merge(nums, l, m, r, res);
+        }
+    }
+    
+    void merge(int[] nums, int l, int m, int r, int[] res) {
+        int x = l, y = m, i = l;
+        while(x < m && y < r) {
+            if(nums[x] < nums[y]) res[i++] = nums[x++];
+            else res[i++] = nums[y++];
+        }
+        
+        while(x < m) res[i++] = nums[x++];
+        
+        while(y < r) res[i++] = nums[y++];
+        
+        for(i = l; i < r; i++) {
+            nums[i] = res[i];
+        }
+    }
+    
 }
