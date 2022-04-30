49e205d528fe6d555d7063b9581e1764db4836cf
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-04-30 08:53:33 +0530

    Time: 4 ms (76.92%), Space: 43.3 MB (69.36%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] alph = new int[26];
+        
+        for(int i = 0; i < s.length(); i++) {
+            alph[s.charAt(i) - 'a']++;
+            alph[t.charAt(i) - 'a']--;
+        }
+        
+        for(int n : alph) {
+            if(n != 0) return false;
+        }
+        
+        return true;
+    }
+}
