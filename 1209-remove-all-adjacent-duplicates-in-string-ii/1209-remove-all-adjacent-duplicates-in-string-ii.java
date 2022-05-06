9ac55db1fe91d3b1fb8940ebaafb6d57da0cd7e0
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-05-06 16:35:16 +0530

    Time: 10 ms (94.33%), Space: 44.3 MB (77.64%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,27 +1,27 @@
 class Solution {
     
-    class Adjacent {
+    class Node {
         char ch;
         int freq;
-        public Adjacent(char ch, int freq) {
+        public Node(char ch, int freq) {
             this.ch = ch;
             this.freq = freq;
         }
     }
     
     public String removeDuplicates(String s, int k) {
-        ArrayDeque<Adjacent> st = new ArrayDeque<>(s.length());
+        ArrayDeque<Node> stack = new ArrayDeque<>(s.length());
         for (char c : s.toCharArray()) {
-            if (!st.isEmpty() && st.peekLast().ch == c) {
-                st.peekLast().freq++; // Increase the frequency
+            if (!stack.isEmpty() && stack.peekLast().ch == c) {
+                stack.peekLast().freq++; // Increase the frequency
             } else {
-                st.addLast(new Adjacent(c, 1));
+                stack.addLast(new Node(c, 1));
             }
-            if (st.peekLast().freq == k) // If reach enough k duplicate letters -> then remove
-                st.removeLast();
+            if (stack.peekLast().freq == k) // If reach enough k duplicate letters -> then remove
+                stack.removeLast();
         }
         StringBuilder sb = new StringBuilder();
-        for (Adjacent a : st) {
+        for (Node a : stack) {
             sb.append(String.valueOf(a.ch).repeat(a.freq));
         }
         return sb.toString();
