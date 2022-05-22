72511ee809b541d113ea3d22ae679748ec2bbb16
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-05-23 01:35:12 +0530

    Time: 21 ms (80.54%), Space: 46.9 MB (69.40%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,39 +1,38 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         if(nums.length == 1) return nums[0] == k ? 1 : 0;
-        int count = 0, sum;
+        int count = 0, sum = 0;
         //Brute force - works - very very slow
-        for(int i = 0; i < nums.length - 1; i++) {
+//         for(int i = 0; i < nums.length - 1; i++) {
             
-            // if(i == nums.length - 1) {
-            //     if(nums[i] == k) count++;
-            //     break;
-            // }
+//             // if(i == nums.length - 1) {
+//             //     if(nums[i] == k) count++;
+//             //     break;
+//             // }
             
-            if(nums[i] == k) {
-                count++;
-            }
+//             if(nums[i] == k) {
+//                 count++;
+//             }
             
-            sum = nums[i];
-            for(int j = i + 1; j < nums.length; j++) {
-                sum += nums[j];
-                if(sum == k) count++;
-            }
-        }
+//             sum = nums[i];
+//             for(int j = i + 1; j < nums.length; j++) {
+//                 sum += nums[j];
+//                 if(sum == k) count++;
+//             }
+//         }
         
-        if(nums[nums.length - 1] == k) count++;
+//         if(nums[nums.length - 1] == k) count++;
         
         //O(n) solution
-//         int sum = 0;
-//         Map<Integer, Integer> map = new HashMap<>();
-//         map.put(0, 1);
-//         for(int n : nums) {
-//             sum += n;
-//             if(map.containsKey(sum - k)) {
-//                 count += map.get(sum - k);
-//             }
-//             map.put(sum, map.getOrDefault(sum, 0) + 1);
-//         }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int n : nums) {
+            sum += n;
+            if(map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
         
         return count;
     }
