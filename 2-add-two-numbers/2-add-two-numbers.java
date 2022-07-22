542798590f33f3809049282e2d598eebebff8481
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-07-22 11:07:08 +0530

    Time: 3 ms (78.30%), Space: 48.1 MB (32.32%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -79,22 +79,35 @@ class Solution {
         
         ListNode res = new ListNode(0);
         
-        ListNode temp = res;
-        int carry = 0, a, b, sum;
-        while(l1 != null || l2 != null) {
-            a = l1 == null ? 0 : l1.val;
-            b = l2 == null ? 0 : l2.val;
-            sum = a + b + carry;
-            carry = sum / 10;
-            temp.next = new ListNode(sum % 10);
-            temp = temp.next;
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
-        }
+//         ListNode temp = res;
+//         int carry = 0, a, b, sum;
+//         while(l1 != null || l2 != null) {
+//             a = l1 == null ? 0 : l1.val;
+//             b = l2 == null ? 0 : l2.val;
+//             sum = a + b + carry;
+//             carry = sum / 10;
+//             temp.next = new ListNode(sum % 10);
+//             temp = temp.next;
+//             if(l1 != null) l1 = l1.next;
+//             if(l2 != null) l2 = l2.next;
+//         }
         
-        if(carry > 0) temp.next = new ListNode(carry);
+//         if(carry > 0) temp.next = new ListNode(carry);
         
+        add(res, l1, l2, 0);
         return res.next;
         
+    }
+    
+    void add(ListNode res, ListNode l1, ListNode l2, int carry) {
+        if(l1 == null & l2 == null) {
+            if(carry > 0) res.next = new ListNode(carry);
+            return;
+        }
+        
+        if(l1 != null) carry += l1.val;
+        if(l2 != null) carry += l2.val;
+        res.next = new ListNode(carry % 10);
+        add(res.next, l1 == null ? l1 : l1.next, l2 == null ? l2 : l2.next, carry / 10);
     }
 }
