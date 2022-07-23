6bdeddb34c847cb5377d5ffbe10063586f831f1d
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-07-24 00:10:22 +0530

    Time: 8 ms (99.41%), Space: 45.1 MB (87.47%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -16,24 +16,54 @@ class Solution {
         // for(int n : list) res[i++] = n; 
         // return res;
         
-        if (k == nums.length) return nums;
+//         if (k == nums.length) return nums;
         
+//         Map<Integer, Integer> map = new HashMap();
+//         for (int n: nums) {
+//             map.put(n, map.getOrDefault(n, 0) + 1);
+//         }
+
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+//             (key1, key2) -> map.get(key1) - map.get(key2));
+
+//         for (int n : map.keySet()) {
+//             minHeap.add(n);
+//             if (minHeap.size() > k) minHeap.poll();    
+//         }
+
+//         int[] res = new int[k];
+//         int i = 0;
+//         for(int n : minHeap) res[i++] = n;
+//         return res;
+        return helper(nums, k);
+    }
+    
+    public int[] helper(int[] nums, int k) {
+        if(k == nums.length) return nums;
         Map<Integer, Integer> map = new HashMap();
         for (int n: nums) {
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
-            (key1, key2) -> map.get(key1) - map.get(key2));
-
-        for (int n : map.keySet()) {
-            minHeap.add(n);
-            if (minHeap.size() > k) minHeap.poll();    
+        
+        List<Integer>[] bucket = new List[nums.length + 1];
+        
+        for(int n : map.keySet()) {
+            int freq = map.get(n);
+            if(bucket[freq] == null) bucket[freq] = new ArrayList<>();
+            bucket[freq].add(n);
         }
-
+        
         int[] res = new int[k];
-        int i = 0;
-        for(int n : minHeap) res[i++] = n;
+        k -= 1;
+        for(int i = nums.length; i >= 0 && k >= 0; i--) {
+            if(bucket[i] != null) {
+                // System.out.println(bucket[i]);
+                int j = 0;
+                for(j = 0; j < bucket[i].size() && k >= 0; j++)  {
+                    res[k--] = bucket[i].get(j);
+                }
+            }
+        }
         return res;
     }
 }
