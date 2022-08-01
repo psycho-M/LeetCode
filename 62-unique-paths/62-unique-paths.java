f18c54f6263c6b325d40c30fa353166ae2e9e1c1
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-01 08:00:39 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (65.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,8 +1,32 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        dp[m - 1][n - 1] = 0;
-        return getPaths(0, 0, dp, m, n);
+        dp[m - 1][n - 1] = 1;
+        
+        for(int i = 0; i < m - 1; i++) dp[i][n - 1] = 1;
+        for(int j = 0; j < n - 1; j++) dp[m - 1][j] = 1;
+        
+        int count = 0;
+        
+        for(int i = m - 2, j = n - 2; i >= 0 && j >= 0;) {
+            int temp = i;
+            
+            while(temp >= 0) {
+                dp[temp][j] = dp[temp + 1][j] + dp[temp][j + 1];
+                temp--;
+            }
+            temp = j - 1;
+            while(temp >= 0) {
+                dp[i][temp] = dp[i + 1][temp] + dp[i][temp + 1];
+                temp--;
+            }
+            
+            j--;
+            i--;
+        }
+        
+        // return getPaths(0, 0, dp, m, n);
+        return dp[0][0];
     }
     
     private int getPaths(int row, int col, int[][] dp, int maxRow, int maxCol) {
