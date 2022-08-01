911396a6671ca51f847c8c088460edd42762acfb
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-01 14:51:40 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (55.79%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -7,24 +7,24 @@ class Solution {
         
     */
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        dp[0] = nums[0];
-        return getMaxRobbery(nums.length - 1, dp, nums);
-//         int n = nums.length;
-//         if(n == 1) return nums[0];
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp, -1);
+//         dp[0] = nums[0];
+//         return getMaxRobbery(nums.length - 1, dp, nums);
+        int n = nums.length;
+        if(n == 1) return nums[0];
         
-//         int prev2 = 0, prev = nums[0];
+        int prev2 = 0, prev = nums[0];
         
-//         for(int i = 1; i < n; i++) {
-//             int take = nums[i] + (i - 2 < 0 ? 0 : prev2);
-//             int notTake = 0 + prev;
-//             int curr = Math.max(take, notTake);
-//             prev2 = prev;
-//             prev = curr;
-//         }
+        for(int i = 1; i < n; i++) {
+            int take = nums[i] + (i - 2 < 0 ? 0 : prev2);
+            int notTake = 0 + prev;
+            int curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
         
-//         return prev;
+        return prev;
     }
     
     private int getMaxRobbery(int idx, int[] dp, int[] nums) {
