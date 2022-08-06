98e3ef1d5fa4d9ede02bca02265fe3691ac71195
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-06 09:34:55 +0530

    Time: 1 ms (92.94%), Space: 41.4 MB (61.50%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,4 +1,22 @@
 class Solution {
+    
+//     public int combinationSum4(int[] nums, int target) {
+//         Arrays.sort(nums);
+//         return getCombinations(target, nums);
+//     }
+    
+//     private int getCombinations(int target, int[] nums) {
+//         if(target == 0) return 1;
+        
+//         int ans = 0;
+        
+//         for(int n : nums) {
+//             if(n <= target) ans += getCombinations(target - n, nums);
+//         }
+        
+//         return ans;
+//     }
+    
     public int combinationSum4(int[] nums, int target) {
         Arrays.sort(nums);
         int[] dp = new int[target + 1];
@@ -12,6 +30,7 @@ class Solution {
         dp[target] = 0;
         for(int n : nums) {
             if(n <= target) dp[target] += getCombinations(target - n, nums, dp);
+            else break;
         }
         return dp[target];
     }
