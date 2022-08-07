b427fb3ca8ff08598ce5b2c8ed7afaebb691772e
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-08 00:16:36 +0530

    Time: 3 ms (47.13%), Space: 44.5 MB (63.74%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -2,12 +2,12 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         // Set<Integer> visited = new HashSet<>();
-        permutations(0, new ArrayList<>(), nums, res);
+        permutations(new ArrayList<>(), nums, res);
         return res;
     }
     
-    public void permutations(int step, List<Integer> curr, int[] nums, List<List<Integer>> res) {
-        if(step == nums.length) {
+    public void permutations(List<Integer> curr, int[] nums, List<List<Integer>> res) {
+        if(curr.size() == nums.length) {
             res.add(new ArrayList<>(curr));
             return;
         }
@@ -17,7 +17,7 @@ class Solution {
                 int currNum = nums[i];
                 curr.add(nums[i]);
                 nums[i] = 11;
-                permutations(step + 1, curr, nums, res);
+                permutations(curr, nums, res);
                 nums[i] = currNum;
                 curr.remove(curr.size() - 1);
             }
