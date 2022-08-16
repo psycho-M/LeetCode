966511c2a6369951cd48fa09ac28158c66f3c21e
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-17 00:56:40 +0530

    Time: 27 ms (41.76%), Space: 44.1 MB (67.97%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,50 +1,93 @@
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         int m = s.length(), n = t.length(), min = m;
+        
+//         if(m < n) return "";
+        
+//         Map<Character, Integer> tFreq = new HashMap<>();
+//         for(char c : t.toCharArray()) tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+//         Map<Character, Integer> sFreq = new HashMap<>();
+//         String res = "";
+//         int l = 0, r = 0;
+//         while(r < m) {
+//             char currR = s.charAt(r);
+//             sFreq.put(currR, sFreq.getOrDefault(currR, 0) + 1);
+//             while(containsAll(tFreq, sFreq)) {
+//                 char c = s.charAt(l);
+                
+//                 if(sFreq.get(c).equals(tFreq.get(c))) {
+//                     if(min > r - l) {
+//                         res = s.substring(l, r + 1);
+//                         min = r - l;
+//                     }
+//                     break;
+//                 } else {
+//                     sFreq.put(c, sFreq.get(c) - 1);
+//                     if(sFreq.get(c) == 0) sFreq.remove(c);
+//                     l++;
+//                 }
+//             }
+//             r++;
+//         }
+        
+//         System.out.println(sFreq);
+//         if(containsAll(tFreq, sFreq) && min > r - l) res = s.substring(l, r);
+        
+//         return res;
+//     }
+    
+//     private boolean containsAll(Map<Character, Integer> tFreq, Map<Character, Integer> sFreq) {
+//         for(char c : tFreq.keySet()) {
+//             if((int)sFreq.getOrDefault(c, 0) < (int)tFreq.get(c)) return false;
+//         }
+
+//         return true;
+//     }
+    
+// }
+
 class Solution {
     public String minWindow(String s, String t) {
         int m = s.length(), n = t.length(), min = m;
         
         if(m < n) return "";
         
-        Map<Character, Integer> tFreq = new HashMap<>();
-        for(char c : t.toCharArray()) tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
-        // System.out.println(tFreq);
-        Map<Character, Integer> sFreq = new HashMap<>();
+        int[] tFreq = new int[123];
+        for(char c : t.toCharArray()) tFreq[c]++;
+        int[] sFreq = new int[123];
         String res = "";
         int l = 0, r = 0;
-        // System.out.println(m);
         while(r < m) {
             char currR = s.charAt(r);
-            sFreq.put(currR, sFreq.getOrDefault(currR, 0) + 1);
-            // System.out.println(sFreq);
+            sFreq[currR]++;
             while(containsAll(tFreq, sFreq)) {
                 char c = s.charAt(l);
                 
-                // System.out.println(l + " " + r);
-                if(sFreq.get(c).equals(tFreq.get(c))) {
+                if(sFreq[c] == tFreq[c]) {
                     if(min > r - l) {
                         res = s.substring(l, r + 1);
                         min = r - l;
-                        // System.out.println(res);
                     }
                     break;
                 } else {
-                    sFreq.put(c, sFreq.get(c) - 1);
-                    if(sFreq.get(c) == 0) sFreq.remove(c);
+                    sFreq[c]--;
+                    // if(sFreq.get(c) == 0) sFreq.remove(c);
                     l++;
                 }
             }
             r++;
         }
         
-        // System.out.println(l + " " + r);
-        System.out.println(sFreq);
+        // System.out.println(sFreq);
         if(containsAll(tFreq, sFreq) && min > r - l) res = s.substring(l, r);
         
         return res;
     }
     
-    private boolean containsAll(Map<Character, Integer> tFreq, Map<Character, Integer> sFreq) {
-        for(char c : tFreq.keySet()) {
-            if((int)sFreq.getOrDefault(c, 0) < (int)tFreq.get(c)) return false;
+    private boolean containsAll(int[] tFreq, int[] sFreq) {
+        for(int i = 65; i < 123; i++) {
+            
+            if(tFreq[i] > 0 && sFreq[i] < tFreq[i]) return false;
         }
 
         return true;
