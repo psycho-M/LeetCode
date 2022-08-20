7df95ae928180e27c68fa0163ebcb4c0bb223d02
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-20 23:15:32 +0530

    Time: 7 ms (13.01%), Space: 50.6 MB (5.27%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -2,39 +2,24 @@ class Solution {
     public int trap(int[] height) {
         ArrayDeque<int[]> stack = new ArrayDeque<>();
         int curr = 0, max = 0;
-        
-        for(int i = 0; i < height.length; i++) {
+        stack.push(new int[]{0, height[0]});
+        for(int i = 1; i < height.length; i++) {
             int h = height[i];
-            if(stack.isEmpty()) {
-                stack.push(new int[]{i, h});
-            } else {
-                if(h < stack.peek()[1]) stack.push(new int[]{i, h});
-                else {
-                    int[] last = null;
-                    while(!stack.isEmpty() && h >= stack.peek()[1]) {
-                        // print(stack);
-                        int[] top = stack.pop();
-                        curr += (top[1] - (last == null ? 0 : last[1])) * (i - top[0] - 1);
-                        last = top;
-                        // System.out.println(last[1]);
-                    }
-                    if(!stack.isEmpty())
-                        curr += (h - (last == null ? 0 : last[1])) * (i - stack.peek()[0] - 1);
-                    // System.out.println(curr);
-                    stack.push(new int[]{i, h});
-                    // print(stack);
+
+            if(h < stack.peek()[1]) stack.push(new int[]{i, h});
+            else {
+                int[] last = null;
+                while(!stack.isEmpty() && h >= stack.peek()[1]) {
+                    int[] top = stack.pop();
+                    curr += (top[1] - (last == null ? 0 : last[1])) * (i - top[0] - 1);
+                    last = top;
                 }
+                if(!stack.isEmpty())
+                    curr += (h - (last == null ? 0 : last[1])) * (i - stack.peek()[0] - 1);
+                stack.push(new int[]{i, h});
             }
         }
         
         return curr;
-    }
-    
-    void print(ArrayDeque<int[]> stack) {
-        System.out.print("[");
-        for(int[] n : stack) {
-            System.out.print(n[1] +", ");
-        }
-        System.out.println("]");
     }
 }
