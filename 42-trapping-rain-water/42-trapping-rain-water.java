94058b3e79d09889757b5a9bf66e268945c48fa6
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-20 23:32:04 +0530

    Time: 1 ms (99.75%), Space: 43.5 MB (86.34%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,25 +1,42 @@
 class Solution {
     public int trap(int[] height) {
-        ArrayDeque<int[]> stack = new ArrayDeque<>();
-        int curr = 0, max = 0;
-        stack.push(new int[]{0, height[0]});
-        for(int i = 1; i < height.length; i++) {
-            int h = height[i];
+//         ArrayDeque<int[]> stack = new ArrayDeque<>();
+//         int curr = 0, max = 0;
+//         stack.push(new int[]{0, height[0]});
+//         for(int i = 1; i < height.length; i++) {
+//             int h = height[i];
 
-            if(h < stack.peek()[1]) stack.push(new int[]{i, h});
-            else {
-                int[] last = null;
-                while(!stack.isEmpty() && h >= stack.peek()[1]) {
-                    int[] top = stack.pop();
-                    curr += (top[1] - (last == null ? 0 : last[1])) * (i - top[0] - 1);
-                    last = top;
-                }
-                if(!stack.isEmpty())
-                    curr += (h - (last == null ? 0 : last[1])) * (i - stack.peek()[0] - 1);
-                stack.push(new int[]{i, h});
+//             if(h < stack.peek()[1]) stack.push(new int[]{i, h});
+//             else {
+//                 int[] last = null;
+//                 while(!stack.isEmpty() && h >= stack.peek()[1]) {
+//                     int[] top = stack.pop();
+//                     curr += (top[1] - (last == null ? 0 : last[1])) * (i - top[0] - 1);
+//                     last = top;
+//                 }
+//                 if(!stack.isEmpty())
+//                     curr += (h - (last == null ? 0 : last[1])) * (i - stack.peek()[0] - 1);
+//                 stack.push(new int[]{i, h});
+//             }
+//         }
+        
+//         return curr;
+        
+        int l = 0, r = height.length - 1;
+        int minH = 0;
+        int total = 0;
+        while(l < r) {
+            if(height[l] <= height[r]) {
+                if(minH > height[l]) total += minH - height[l];
+                else minH = height[l];
+                l++;
+            } else {
+                if(minH > height[r]) total += minH - height[r];
+                else minH = height[r];
+                r--;
             }
         }
         
-        return curr;
+        return total;
     }
 }
