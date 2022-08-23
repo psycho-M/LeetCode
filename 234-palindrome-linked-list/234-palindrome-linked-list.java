dc1c597ea072c1ab2d390012cb47639137ffadf8
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-23 11:25:12 +0530

    Time: 6 ms (79.16%), Space: 96.9 MB (75.29%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,28 +10,51 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        if(head.next == null) return true;
         int n = 1;
         ListNode slow = head, fast = head;
-        Deque<ListNode> stack = new ArrayDeque<>();
+        // Deque<ListNode> stack = new ArrayDeque<>();
         while(fast.next != null) {
             if(fast.next.next == null) {
-                stack.push(slow);
+                // stack.push(slow);
                 n++;
                 break;
             }
-            stack.push(slow);
+            // stack.push(slow);
             slow = slow.next;
             fast = fast.next.next;
             n += 2;
         }
-        // System.out.println(slow.val);
-        slow = slow.next;
-        // System.out.println(stack);
-        // if((n & 1) == 1) stack.pop();
+        ListNode secondPart = slow.next;
+        // System.out.println(secondPart.val);
+        slow.next = null;
         
-        while(slow != null) {
-            if(stack.pop().val != slow.val) return false;
-            slow = slow.next;
+        ListNode prev = null, curr = secondPart, next = secondPart.next;
+        
+        while(curr != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            if(curr == null) break;
+            next = next.next;            
+        }
+        secondPart = prev;
+        // System.out.println(secondPart.val);
+        ListNode temp = secondPart;
+        // while(temp != null) {
+        //     System.out.print(temp.val + "->");
+        //     temp = temp.next;
+        // }
+        // while(slow != null) {
+        //     if(stack.pop().val != slow.val) return false;
+        //     slow = slow.next;
+        // }
+        
+        curr = head;
+        for(int i = 0; i < n / 2; i++) {
+            if(curr.val != secondPart.val) return false;
+            curr = curr.next;
+            secondPart = secondPart.next;
         }
         
         return true;
