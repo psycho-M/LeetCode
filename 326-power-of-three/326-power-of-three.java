8a6b5f1cf0f5b1bd7f38dea8c2d83b976c47be2e
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-08-24 23:16:20 +0530

    Time: 23 ms (54.61%), Space: 46.9 MB (74.59%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        // if(n == 1) return true;
-        // if(n == 0) return false;
+        if(n == 1) return true;
+        if(n == 0) return false;
         // double check = Math.log(n) / Math.log(3);
         // System.out.println(check);
-        // return check == (int) check;
-        // for(int i = 1; i <= n; i *= 3) {
-        //     if(i == n) return true;
-        // }
+        // return (check + 1) % 1 <= 2 * 1;
+        long temp = 1;
+        for(int i = 1; i <= 19; i++) {
+            temp *= 3;
+            if(temp == n) return true;
+        }
+        return false;
         // return false;
-        String thirdBase = Integer.toString(n, 3);
-        boolean res = thirdBase.matches("^10*$");
-        return res;
+        // String thirdBase = Integer.toString(n, 3);
+        // boolean res = thirdBase.matches("^10*$");
+        // return res;
     }
 }
