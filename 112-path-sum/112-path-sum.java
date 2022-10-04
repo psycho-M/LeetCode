f9be103c966de169e063718a1355bc027691dad6
psycho-M <61588507+psycho-M@users.noreply.github.com>
2022-10-04 07:08:59 +0530

    Time: 1 ms (69.13%), Space: 43.6 MB (51.42%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -18,7 +18,6 @@ class Solution {
         if(root == null) return false;
         if(root.left == null && root.right == null) return targetSum - root.val == 0;
         return hasPathSum(root.left, targetSum - root.val) || 
-            hasPathSum(root.right, targetSum - root.val);
-        
+            hasPathSum(root.right, targetSum - root.val);   
     }
 }
